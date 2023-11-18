595f0d0dc8eafbe22c9262e9af62ff0fb38fe897
Noor <khannooralam31@gmail.com>
2023-11-18 17:00:16 +0530

    -

--- lib/bird.dart
+++ lib/bird.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 
 class MyBird extends StatelessWidget {
   final birdY;
+  final double birdWidth; // normal double value for width.
+  final double birdHeight; // out of 2, 2 being the entire height of the screen
 
-  const MyBird({super.key, this.birdY});
+  const MyBird({super.key, this.birdY, required this.birdWidth, required this.birdHeight});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment(0, birdY),
-      width: 50,
-      height: 50,
-      child: Image.asset('asset/images/bird1.png'),
-    );
+        alignment: Alignment(0, (2 * birdY + birdHeight) / (2 - birdHeight)),
+        child: Image.asset(
+          'images/bird.png',
+          width: MediaQuery.of(context).size.height * birdWidth / 2,
+          height: MediaQuery.of(context).size.height * 3 / 4 * birdHeight / 2,
+          fit: BoxFit.fill,
+        ));
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,59 +1,277 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-
+import 'barrier.dart';
 import 'bird.dart';
+import 'cover_screen.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdY = 0;
-  void jump() {
-    Timer.periodic(const Duration(milliseconds: 50), (timer) {
+  // bird variables
+  static double birdY = 0;
+  double initialPos = birdY;
+  double height = 0;
+  double time = 0;
+  double gravity = -4.9; // how strong the gravity is
+  double velocity = 3.5; // how strong the jump is
+  double birdWidth = 0.1; // out of 2, 2 being the entire width of the screen
+  double birdHeight = 0.1; // out of 2, 2 being the entire height of the screen
+  int score = 0;
+
+  // game settings
+  bool gameHasStarted = false;
+
+  // barrier variables
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.5; // out of 2
+  List<List<double>> barrierHeight = [
+    // out of 2, where 2 is the entire height of the screen
+    // [topHeight, bottomHeight]
+    [0.6, 0.4],
+    [0.4, 0.6],
+  ];
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(const Duration(milliseconds: 60), (timer) {
+      // a real physical jump is the same as an upside down parabola
+      // so this is a simple quadratic equation
+      // height = gravity * time * time + velocity * time;
+
+      height = -4.9 * time * time + 2.8 * time;
+
       setState(() {
-        birdY -= 0.05;
-        print("object");
+        birdY = initialPos - height;
       });
+
+      // check if bird is dead
+      if (birdIsDead()) {
+        timer.cancel();
+        _showDialog();
+      }
+
+      // keep the map moving (move barriers)
+      moveMap();
+      // keep the time going!
+      time += 0.05;
     });
+  }
+
+  void moveMap() {
+  for (int i = 0; i < barrierX.length; i++) {
+    // keep barriers moving
+    setState(() {
+      barrierX[i] -= 0.04;
+    });
+
+    // if barrier exits the left part of the screen, keep it looping
+    if (barrierX[i] < -1.5) {
+      barrierX[i] += 2.8;
+    }
+
+    // Check for successful passage and increase the score
+    if (barrierX[i] + barrierWidth < -birdWidth) {
+      score += 1;
+    }
+  }
+}
+
+
+  // void moveMap() {
+  //   for (int i = 0; i < barrierX.length; i++) {
+  //     // keep barriers moving
+  //     setState(() {
+  //       barrierX[i] -= 0.04;
+  //     });
+
+  //     // if barrier exits the left part of the screen, keep it looping
+  //     if (barrierX[i] < -1.5) {
+  //       barrierX[i] += 2.8;
+  //     }
+  //   }
+  // }
+
+  void resetGame() {
+    Navigator.pop(context); // dismisses the alert dialog
+    setState(() {
+      birdY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = birdY;
+      barrierX = [2, 2 + 1.5];
+      score = 0; // Reset the score
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.brown,
+            title: const Center(
+              child: Text(
+                "G A M E  O V E R",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            actions: [
+              GestureDetector(
+                onTap: resetGame,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Container(
+                    padding: const EdgeInsets.all(7),
+                    color: Colors.white,
+                    child: const Text(
+                      'PLAY AGAIN',
+                      style: TextStyle(color: Colors.brown),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+
+  bool birdIsDead() {
+    // check if the bird is hitting the top or the bottom of the screen
+    if (birdY < -1 || birdY > 1) {
+      return true;
+    }
+
+    // hits barriers
+    // checks if bird is within x coordinates and y coordinates of barriers
+    for (int i = 0; i < barrierX.length; i++) {
+      if (barrierX[i] <= birdWidth &&
+          barrierX[i] + barrierWidth >= -birdWidth &&
+          (birdY <= -1 + barrierHeight[i][0] ||
+              birdY + birdHeight >= 1 - barrierHeight[i][1])) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: jump,
+      onTap: gameHasStarted ? jump : startGame,
       child: Scaffold(
         body: Column(
           children: [
             Expanded(
-                flex: 3,
-                child: Container(
-                  color: Colors.blue,
-                  child: Center(
-                    child: Stack(
-                      children: [
-                        MyBird(birdY: birdY),
-                        // Container(
-                        //   alignment: Alignment(0, birdY),
-                        //   child: Container(
-                        //     height: 50,
-                        //     width: 50,
-                        //     color: Colors.yellow,
-                        //     child: Image.asset('asset/images/bird1.png'),
-                        //   ),
-                        // )
-                      ],
-                    ),
+              flex: 4,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      // bird
+                      MyBird(
+                        birdY: birdY,
+                        birdWidth: birdWidth,
+                        birdHeight: birdHeight,
+                      ),
+
+                      // tap to play
+                      MyCoverScreen(gameHasStarted: gameHasStarted),
+
+                      // Top barrier 0
+                      MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[0][0],
+                        isThisBottomBarrier: false,
+                      ),
+
+                      // Bottom barrier 0
+                      MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[0][1],
+                        isThisBottomBarrier: true,
+                      ),
+
+                      // Top barrier 1
+                      MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[1][0],
+                        isThisBottomBarrier: false,
+                      ),
+
+                      // Bottom barrier 1
+                      MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[1][1],
+                        isThisBottomBarrier: true,
+                      ),
+                    ],
                   ),
-                )),
+                ),
+              ),
+            ),
             Expanded(
-                child: Container(
-              color: Colors.brown,
-            )),
+              child: Container(
+                color: Colors.brown,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            score.toString(),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 35),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const Text(
+                            'T I M E',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      // const Column(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: [
+                      //     Text(
+                      //       '10',
+                      //       style: TextStyle(color: Colors.white, fontSize: 35),
+                      //     ),
+                      //     SizedBox(
+                      //       height: 15,
+                      //     ),
+                      //     Text(
+                      //       'B E S T',
+                      //       style: TextStyle(color: Colors.white, fontSize: 20),
+                      //     ),
+                      //   ],
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
